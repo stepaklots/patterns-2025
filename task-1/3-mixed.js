3a5ccd468ab4015c5373279b5e3daa0a5f007995
Stepan Dediukhin <stepan.dediukhin@gmail.com>
2025-05-21 14:45:46 +0300

    add mixed approach and test

--- task-1/3-mixed.js
+++ task-1/3-mixed.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const data = `city,population,area,density,country
   Shanghai,24256800,6340,3826,China
   Delhi,16787941,1484,11313,India
@@ -10,4 +12,145 @@ const data = `city,population,area,density,country
   New York City,8537673,784,10892,United States
   Bangkok,8280925,1569,5279,Thailand`;
 
+const PadAlign = {
+  'left': 'left',
+  'right': 'right',
+}
 
+const DataPadConfig = {
+  city: {
+    name: 'city',
+    length: 18,
+    align: PadAlign.left,
+  },
+  population: {
+    name: 'population',
+    length: 10,
+    align: PadAlign.right,
+  },
+  area: {
+    name: 'area',
+    length: 8,
+    align: PadAlign.right,
+  },
+  density: {
+    name: 'density',
+    length: 8,
+    align: PadAlign.right,
+  },
+  country: {
+    name: 'country',
+    length: 18,
+    align: PadAlign.right,
+  },
+  areaNormalized: {
+    name: '% max area',
+    length: 6,
+    align: PadAlign.right,
+  },
+}
+
+class CsvData {
+  #input;
+  #withHeader;
+
+  constructor({ input, withHeader }) {
+    this.#input = input;
+    this.#withHeader = withHeader;
+  }
+
+  parse() {
+    let header = [];
+    let rows = [];
+    const lines = this.#input.split('\n');
+    if (this.#withHeader) {
+      header = lines[0].split(',');
+      lines.shift();
+    }
+    for (const line of lines) {
+      const cells = line.split(',');
+      rows.push(cells);
+    }
+    return { header, rows };
+  }
+}
+
+class Table {
+  #header;
+  #rows = [];
+
+  constructor({ header, rows }) {
+    this.#header = header;
+    this.#rows = rows;
+  }
+
+  normalize({ source, destination }) {
+    const sourceIndex = this.#header.indexOf(source);
+    const maxValue = this.#rows
+      .map(row => row[sourceIndex])
+      .reduce((a, b) => Math.max(a, b), -Infinity);
+
+    for (const row of this.#rows) {
+      const relativePercentage = (row[sourceIndex] / maxValue) * 100;
+      const rounded = Math.round(relativePercentage);
+      row.push(rounded.toString());
+    }
+    this.#header.push(destination);
+    return this;
+  }
+
+  sort({ columnName }) {
+    const sortIndex = this.#header.indexOf(columnName);
+    if (!sortIndex) return this;
+    this.#rows = this.#rows.sort((r1, r2) => r2[sortIndex] - r1[sortIndex]);
+    return this;
+  }
+
+  pad({ config }) {
+    const foundPadding = this.#header
+      .map(h => config.find(c => c.name === h));
+    for (const row of this.#rows) {
+      for (let i = 0; i < this.#header.length; i++) {
+        const pad = foundPadding[i];
+        if (pad && pad.align === PadAlign.left)
+          row[i] = row[i].padEnd(pad.length);
+        if (pad && pad.align === PadAlign.right)
+          row[i] = row[i].padStart(pad.length);
+      }
+    }
+    return this;
+  }
+
+  print() {
+    for (const row of this.#rows) {
+      console.log(row.join(''));
+    }
+  }
+}
+
+const csvData = new CsvData({
+  input: data,
+  withHeader: true,
+});
+const table = new Table(csvData.parse());
+
+table
+  .normalize({
+    source: DataPadConfig.density.name,
+    destination: DataPadConfig.areaNormalized.name,
+  })
+  .sort({
+    columnName: DataPadConfig.areaNormalized.name
+  })
+  .pad({
+    config: Object.values(DataPadConfig),
+  })
+  .print();
+
+
+module.exports = {
+  CsvData,
+  Table,
+  DataPadConfig,
+  PadAlign,
+}
---
+++ task-1/3-mixed.test.js
@@ -0,0 +1,57 @@
+const { Table, CsvData, DataPadConfig, PadAlign } = require('./3-mixed');
+
+/**
+ * Test area
+ */
+
+const randString = (length = 10) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
+const generateRandomNumber = (min = 1, max = 100) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let testData = 'city,population,area,density,country';
+const linesCount = 1000000;
+
+for (let i = 0; i < linesCount; i++) {
+  const city = randString(10);
+  const population = generateRandomNumber(100000000, 1000000000);
+  const area = generateRandomNumber(100000, 1000000);
+  const density = generateRandomNumber(1000, 10000);
+  const country = randString(10);
+  const line = `${city},${population},${area},${density},${country}`;
+  testData += `\n${line}`;
+}
+
+console.log(`START TEST`);
+const startTime = new Date();
+
+const csvTestData = new CsvData({
+  input: testData,
+  withHeader: true,
+});
+new Table(csvTestData.parse())
+  .normalize({
+    source: 'density',
+    destination: '% max area',
+  })
+  .sort({
+    columnName: '% max area',
+  })
+  .pad({
+    config: Object.values(DataPadConfig)
+  })
+  .print();
+
+const elapsed = new Date() - startTime;
+console.log(`END TEST`);
+console.log(`elapsed: ${elapsed} ms`);
+console.log(`data length: ${linesCount}`);
